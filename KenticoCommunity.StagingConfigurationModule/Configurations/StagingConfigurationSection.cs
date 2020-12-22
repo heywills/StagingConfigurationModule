e41b3af8e75319f323bb8bb3ba32bce591951019
Mike Wills <mwills@bluemodus.com>
2020-12-21 21:06:56 -0800

    Restoring more deleted files for legacy config files

---
+++ KenticoCommunity.StagingConfigurationModule/Configurations/StagingConfigurationSection.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace KenticoCommunity.StagingConfigurationModule.Configurations
+{
+    /// <summary>
+    /// A configuration section for containing settings for the Staging Configuration Module.
+    /// </summary>
+    public class StagingConfigurationSection : ConfigurationSection
+    {
+        public const string StagingConfigurationSectionName = "stagingConfiguration";
+        public const string SourceServerSectionName = "sourceServer";
+        public const string TargetServerSectionName = "targetServer";
+
+        [ConfigurationProperty(SourceServerSectionName)]
+        public SourceServerElement SourceServerElement =>
+            this[SourceServerSectionName] as SourceServerElement;
+
+        [ConfigurationProperty(TargetServerSectionName)]
+        public TargetServerElement TargetServerSection =>
+            this[TargetServerSectionName] as TargetServerElement;
+    }
+}
